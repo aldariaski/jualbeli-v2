aef758b8d813f0aee129601872c1aec8a4280aeb
aldariaski <aldrianyf@gmail.com>
2026-09-16 13:59:50 +0700

    int coba handle

--- jualbeli_backend/lib/models/user.dart
+++ jualbeli_backend/lib/models/user.dart
@@ -15,7 +15,7 @@ class User {
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
-      id: map['Id'],
+      id: int.parse((map['Id']).toString()),
       name: map['Name'] as String,
       email: map['Email'] as String,
       passwordHash: map['PasswordHash'] as String,
