d10b8c096af3d0d33a6ad474a1b1b8c53e898344
aldariaski <58584870+aldariaski@users.noreply.github.com>
2026-09-16 07:40:14 +0700

    vercel

--- jualbeli_backend/bin/jualbeli_backend.dart
+++ jualbeli_backend/bin/jualbeli_backend.dart
@@ -5,12 +5,12 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
 
-import 'package:jualbeli_backend/database/database.dart';
-import 'package:jualbeli_backend/routes/auth_routes.dart';
-import 'package:jualbeli_backend/routes/product_routes.dart';
-import 'package:jualbeli_backend/routes/cart_routes.dart';
-import 'package:jualbeli_backend/routes/order_routes.dart';
-import 'package:jualbeli_backend/middleware/auth_middleware.dart';
+import '../database/database.dart';
+import '../routes/auth_routes.dart';
+import '../routes/product_routes.dart';
+import '../routes/cart_routes.dart';
+import '../routes/order_routes.dart';
+import '../middleware/auth_middleware.dart';
 
 Future<void> main() async {
   final database = DatabaseConnection.instance;
