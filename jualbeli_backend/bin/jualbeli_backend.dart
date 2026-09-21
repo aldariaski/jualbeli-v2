651f3c59965ad93ceb217401b7460e02dfdf68b1
aldariaski <aldrianyf@gmail.com>
2026-09-21 14:40:24 +0700

    pay

--- jualbeli_backend/bin/jualbeli_backend.dart
+++ jualbeli_backend/bin/jualbeli_backend.dart
@@ -10,6 +10,7 @@ import 'package:jualbeli_backend/routes/auth_routes.dart';
 import 'package:jualbeli_backend/routes/product_routes.dart';
 import 'package:jualbeli_backend/routes/cart_routes.dart';
 import 'package:jualbeli_backend/routes/order_routes.dart';
+import 'package:jualbeli_backend/routes/payment_routes.dart';
 import 'package:jualbeli_backend/middleware/auth_middleware.dart';
 
 Future<void> main() async {
@@ -69,6 +70,13 @@ Future<void> main() async {
       Pipeline()
           .addMiddleware(AuthMiddleware.middleware)
           .addHandler(OrderRoutes.instance.router.call),
+    );
+
+    router.mount(
+      '/payments/',
+      Pipeline()
+          .addMiddleware(AuthMiddleware.middleware)
+          .addHandler(PaymentRoutes.instance.router.call),
     );
 
     final handler = Pipeline()
